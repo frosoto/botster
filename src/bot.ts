7b0bca34d422c0ded9f7dbeb07dad9113149ee7d
frosoto <totallynotlondon@gmail.com>
2026-05-17 21:33:14 -0500

    better :3

--- src/bot.ts
+++ src/bot.ts
@@ -242,7 +242,7 @@ client.on("messageCreate", async msg => {
     }
 
     // :3
-    if (msg.content.endsWith(" :3") || msg.content.endsWith(" >:3") && get_opted(msg.author.id)) {
+    if (args[args.length] === ":3" || args[args.length] === ">:3" && get_opted(msg.author.id)) {
         respond(msg, ":3", false, [])
     }
 
